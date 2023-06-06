17eeb6e78b76aa00bf13f5bccdb1d872305c3791
IdaAlessandra ScottodiLuzio <idaalessandra.scottodiluzio@gmail.com>
2023-06-06 17:24:27 +0200

    carosello img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,12 @@
      <!-- carosel -->
   <section>
         <div class="overShadow"></div>
-        <div id="carouselExampleInterval" class="carousel slide carosel-custom" data-bs-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide carosel-custom" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
           <div class="carousel-inner">
             <div class="carousel-item active vh-100" data-bs-interval="10000">
               <img class="foto1 img-fluid d-block w-100" src="/media/foto1.jpg"  alt="foto1">
@@ -12,17 +17,9 @@
               <img src="/media/foto2.jpg" class="d-block w-100" alt="foto2">
             </div>
             <div class="carousel-item vh-100">
-              <img src="/media/foto3.jpg" class="d-block w-100" alt="foto3">
+              <img src="/media/foto3.jpg" class="d-block w-100 foto3" alt="foto3">
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
         </div>
       </section>
 
