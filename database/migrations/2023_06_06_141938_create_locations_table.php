5789960e5f81eed8c10cda2cfb21ed8ee3912dd4
IdaAlessandra ScottodiLuzio <idaalessandra.scottodiluzio@gmail.com>
2023-06-07 10:09:34 +0200

    aggiunto onDelete corretto probelma table locations

--- database/migrations/2023_06_06_141938_create_locations_table.php
+++ database/migrations/2023_06_06_141938_create_locations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
