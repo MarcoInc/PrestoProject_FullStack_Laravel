8435a38aad048ed69978b8aa56584b0ce382fb3c
MarcoInc <soteramarco@gmail.com>
2023-06-08 10:21:27 +0200

    m

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -4,10 +4,10 @@
     <div class="row">
         <div class="col-12">
             <h2>{{$house->place}}</h2>
-            <h2>{{$house->name}}</h2>
-            <h2>{{$house->beds}}</h2>
-            <h2>{{$house->price}}</h2>
+            <h3>Posti letto : {{$house->beds}}</h3>
+            <h2>Prezzo a notte : {{$house->price}}</h2>
             <img src="{{Storage::url($house->img)}}" alt="{{$house->name}}">
+        
             @if(Auth::id()==$house->user_id)
                     <a href="{{ route('edit',compact('house')) }}" class="btn btn-warning">Modifica</a>
                     <form method=POST action={{route('delete',compact('house'))}}>
