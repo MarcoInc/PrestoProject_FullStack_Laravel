b8e75b7fa42aa751261357ecdc81190402ddaf80
IdaAlessandra ScottodiLuzio <idaalessandra.scottodiluzio@gmail.com>
2023-06-14 12:16:11 +0200

    parentesi

--- app/Http/Livewire/CreateForm.php
+++ app/Http/Livewire/CreateForm.php
@@ -9,7 +9,8 @@ use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Auth;
 
 
-class CreateForm extends Component{
+class CreateForm extends Component
+{
     public $place, $price, $description, $beds, $location_id, $user_id, $guest_houses, $img;
     public $images = [];
     public $temporary_images;
@@ -53,8 +54,9 @@ class CreateForm extends Component{
                 foreach ($this->temporary_images as $image){
                     $this->images[] = $image;
                 }
+            }
+            
         }
-    
     }
     
     public function removeImage($key)
@@ -86,26 +88,8 @@ class CreateForm extends Component{
         session()->flash('message', 'Prodotto caricato correttamente');
         
         $this->reset();
-        
-        
-        
-<<<<<<< HEAD
-        
-        
-        
-    }
+    }    
     
-    public function cleanForm()
-    {
-        $this->place = '';
-        $this->beds = '';
-        $this->price = '';
-        $this->description= '';
-        $this->image = '';
-        $this->images = [];
-        $this->temporary_images = [];
-        
-    }
     
     public function updated($propertyName){
         $this->validateOnly($propertyName);
@@ -114,16 +98,5 @@ class CreateForm extends Component{
         return view('livewire.create-form', ['locations'=> Location::all()]);
         // return view('livewire.show-form', ['colors'=> Color::all(),'genres'=> Genre::all()]);
     }
-}
-     
-=======
-        public function updated($propertyName){
-            $this->validateOnly($propertyName);
-        }
-        public function render(){
-            return view('livewire.create-form', ['locations'=> Location::all()]);
-            // return view('livewire.show-form', ['colors'=> Color::all(),'genres'=> Genre::all()]);
-        }
     
 }        
->>>>>>> 69956b99b2e31c215020a20a74115ea6f0e1d367
