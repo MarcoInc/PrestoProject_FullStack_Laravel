6091a1ed930a0e8a2f14b049fce468ab7332e7da
MarcoInc <soteramarco@gmail.com>
2023-06-19 14:23:08 +0200

    translate component

--- resources/views/product/category-show.blade.php
+++ resources/views/product/category-show.blade.php
@@ -9,55 +9,9 @@
             <div class="col-12">
                 <h2 class="pt-5">{{__('ui.explorerCategory')}}: 
                     {{-- {{$location->name}} --}}
-                    @if(App::getLocale()=='it')
-                        @if($location->name=='Mare')
-                            Mare
-                        @elseif($location->name=='Montagna')
-                            Montagna
-                        @elseif($location->name=='Lago')
-                            Lago
-                        @elseif($location->name=='Deserto')
-                            Deserto
-                        @elseif($location->name=='Campagna')
-                            Campagna
-                        @elseif($location->name=='Citta')
-                            Città
-                        @elseif($location->name=='Neve')
-                            Neve
-                        @endif
-                    @elseif(App::getLocale()=='en')
-                        @if($location->name=='Mare')
-                            Beach
-                        @elseif($location->name=='Montagna')
-                            Mountain
-                        @elseif($location->name=='Lago')
-                            Lake
-                        @elseif($location->name=='Deserto')
-                            Desert
-                        @elseif($location->name=='Campagna')
-                            Countryside
-                        @elseif($location->name=='Citta')
-                            City
-                        @elseif($location->name=='Neve')
-                            Snow
-                        @endif
-                    @elseif(App::getLocale()=='es')
-                        @if($location->name=='Mare')
-                            Mar
-                        @elseif($location->name=='Montagna')
-                            Montaña
-                        @elseif($location->name=='Lago')
-                            Lago
-                        @elseif($location->name=='Deserto')
-                            Desierto
-                        @elseif($location->name=='Campagna')
-                            Campo
-                        @elseif($location->name=='Citta')
-                            Ciudad
-                        @elseif($location->name=='Neve')
-                        Nieve
-                        @endif
-                    @endif
+                    
+                    <x-locationTranslateCategory :location="$location"/>         
+
                 </h2>
             </div>
         </div>
